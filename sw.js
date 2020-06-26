3a96c926cf1d5d723dbc5dc9e08f1d4e7fe40b31
relunna <67445394+relunna@users.noreply.github.com>
2020-06-26 16:56:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
+var cacheName = 'Jeu-morpion';
+var filesToCache = [ //3 fichiers à installer sur le téléphone
   '/',
   '/index.html',
   '/tic-tac-toe.css',
   '/tic-tac-toe.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
+/* Mettre en cache tous les fichiers */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -15,7 +15,8 @@ self.addEventListener('install', function(e) {
   );
 });
 
-/* Serve cached content when offline */
+/* il écoute l'évènement fetch. Si le résulat est nul alors on récupère ce qu'il y a dans le cache sinon on récupère depuis le site internet 
+C'est le mode offline */
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
